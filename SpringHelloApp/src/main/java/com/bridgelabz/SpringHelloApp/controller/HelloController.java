77403e9a1a4e0e8b150a49bf3cabd276a7ed3d79
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-05-30 19:23:35 +0530

    Added:GET Request Method and Pass Name As query parameter

--- SpringHelloApp/src/main/java/com/bridgelabz/SpringHelloApp/controller/HelloController.java
+++ SpringHelloApp/src/main/java/com/bridgelabz/SpringHelloApp/controller/HelloController.java
@@ -1,17 +1,40 @@
 package com.bridgelabz.SpringHelloApp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-
+    // UC 1
+//    Create a Rest Controller to demonstrate
+//     the various HTTP Methods and respond
+//     hello messages to the User. To begin
+//     with show Hello from BridgeLabz
+//       Use GET Request Method
+        @GetMapping(value={"/hello"})
         @RequestMapping(value = {"/hello"}, method = RequestMethod.GET)
         public String sayHello() {
             return "Hello from BridgeLabz !!!";
         }
+
+       //UC 2
+
+//    Make REST Call to show Hello
+//     * Mark from BridgeLabz
+//     * Use GET Request Method and pass name as
+//     * query parameter
+//     * - Use CURL to demonstrate the REST API Call
+//    @RequestMapping(value = "/query",method = RequestMethod.GET)
+
+    public String sayHello(@RequestParam String name){
+        return "Hello "+ name +" from bridgelabz";
+    }
+    @GetMapping( {"/query1"})
+    public String sayHelloQuey(@RequestParam String name){
+        return "Hello "+ name +" from bridgelabz";
+    }
+
+
     }
 
 
